2cda23179b4c2a04ca9ee2f3a48c0114888192d2
Brian Summerfield <brianjsummerfield@gmail.com>
2019-02-21 21:29:52 -0600

    Added multiple definitions

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -1,11 +1,10 @@
 require 'http'
-
 @complete = true
-puts "Enter a word:"
 puts "Dictionary"
+puts "Enter a word:"
 
 def dictionary
-  word = gets.chomp
+  word = gets.chomp.downcase
   word_data =  HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=")
   @word_data = word_data.parse
 end
@@ -13,7 +12,9 @@ end
  def define
   puts "-------------------"
   puts @word_data[0]["word"]
-  puts @word_data[0]["text"]
+    @word_data.length.times do |i|
+      puts "#{i+1}.) #{@word_data[i]["text"]}"
+    end
   @complete = false
 end
 
